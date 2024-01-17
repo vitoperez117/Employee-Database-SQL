38303eea9378cef8fc1ffe15a93f8716f9e02f42
Vito Perez <vitoperez117@gmail.com>
2024-01-16 20:41:10 -0800

    Add code to import csv files into postgresql

--- Employee_SQL/table_schemata.sql
+++ Employee_SQL/table_schemata.sql
@@ -34,3 +34,30 @@ create table titles (
 	title varchar,
 	from_date date,
 	to_date date);
+
+----Import CSV files into postgres database (vito/postgres@employeeDatabase)
+
+COPY departments (dept_no, dept_name)
+FROM '/Users/vito/Desktop/HW BootCamp/9-sql-challenge/CSV files/departments.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY dept_emp (emp_no, dept_no, from_date, to_date)
+FROM '/Users/vito/Desktop/HW BootCamp/9-sql-challenge/CSV files/dept_emp.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY dept_manager (dept_no, emp_no, from_date, to_date)
+FROM '/Users/vito/Desktop/HW BootCamp/9-sql-challenge/CSV files/dept_manager.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY employees (emp_no, birth_date, first_name, last_name, gender, hire_date)
+FROM '/Users/vito/Desktop/HW BootCamp/9-sql-challenge/CSV files/employees.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY salaries (emp_no, salary, from_date, to_date)
+FROM '/Users/vito/Desktop/HW BootCamp/9-sql-challenge/CSV files/salaries.csv'
+DELIMITER ','
+CSV HEADER;
